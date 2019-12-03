c7fe8ca426f50120357f9302b44480b7788aa611
Sai Sidharth Giridharan <gsaisidharth@gmail.com>
2019-12-04 03:38:31 +0530

    Updates

---
+++ precache-manifest.161e54655ec7cec08fa0479c8da92ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2829028d015f6a8da17c02464b7243",
+    "url": "/chat-fe/index.html"
+  },
+  {
+    "revision": "a9b80386cdd317a8a32b",
+    "url": "/chat-fe/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "5229b84128f21989c684",
+    "url": "/chat-fe/static/js/2.2b750f4e.chunk.js"
+  },
+  {
+    "revision": "a9b80386cdd317a8a32b",
+    "url": "/chat-fe/static/js/main.0c676175.chunk.js"
+  },
+  {
+    "revision": "229db241804525217144",
+    "url": "/chat-fe/static/js/runtime-main.5e373323.js"
+  }
+]);
